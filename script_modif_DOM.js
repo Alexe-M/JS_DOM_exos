c84e3fe3f7fb4e6e026411baee97ed4af1e0f029
Alexe Marichal <marichalalexe@gmail.com>
2024-11-16 12:55:05 +0200

    ajout des solutions jusque exos 7

--- script_modif_DOM.js
+++ script_modif_DOM.js
@@ -56,7 +56,7 @@ console.log("********************Modification n-4")
     "https://img.icons8.com/color/480/000000/ruby-programming-language.png",
     "https://img.icons8.com/color/480/000000/bootstrap.png",
     "https://img.icons8.com/color/480/000000/github.png",
-    "https://i.imgur.com/bJE9Pka.png",
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Druby%2BOn%2BRails&psig=AOvVaw1xJYEd55sYCm8TpuKiUAs1&ust=1731839748082000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjH2P3T4IkDFQAAAAAdAAAAABAI",
     "https://avatars2.githubusercontent.com/u/25484553?s=200&v=4",
     "https://img.icons8.com/color/480/000000/heroku.png"
   ];
@@ -79,7 +79,7 @@ populateImages()
 console.log("********************Modification n-5")
 function deleteLastCards() {
   let elementCards = document.querySelectorAll(".card")
-
+  console.log(elementCards)
   // Supprimer les 3 dernières cards
   for(let i=elementCards.length - 1; i >= elementCards.length - 3; i--) {
     elementCards[i].remove(); // Supprimer chaque card
@@ -90,10 +90,44 @@ deleteLastCards()
 
 //Modification n°6:
 console.log("********************Modification n-6")
+function changeCardsText() {
+  let elementCardText = document.querySelectorAll(".card-text");
+  console.log(elementCardText);
+
+  let newText = [
+    "L’HyperText Markup Language, généralement abrégé HTML, est le langage de balisage conçu pour représenter les pages web",
+    "Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML", 
+    "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs. C'est un langage orienté objet à prototype." 
+  ];
+
+  if (elementCardText.length < 3) {
+    console.error("Il n'y a pas assez de cards pour modifier les textes !");
+    return;
+  }
+  let index = 0;
+  for(let i=0; i < 3; i++) { //i contrôle les éléments de elementCardText de 0 à 2.
+    elementCardText[i].innerHTML = newText[index] //index contrôle les éléments de newText : il va également de 0 à 2.
+    index++;
+  }
+    
+}
+changeCardsText()
 
 
-//Écris une fonction changeCardsText() qui va modifier le texte des 3 premières cards et le remplacer avec cela :
-// Carte HTML : "L’HyperText Markup Language, généralement abrégé HTML, est le langage de balisage conçu pour représenter les pages web"
-// Carte CSS : "Les feuilles de style en cascade, généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML"
-// Carte JS : "JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives mais aussi pour les serveurs. C'est un langage orienté objet à prototype."
-// Essaye de mettre ça dans une boucle.
+//Modification n°7
+console.log("********************Modification n-7")
+function changeViewButtons() {
+  let elementBtnGroups = document.querySelectorAll(".btn-group");
+  
+  elementBtnGroups.forEach(group => {
+    let viewButton = group.children[0]; //Premier bouton du button-group
+    console.log(viewButton);
+
+    viewButton.classList.remove("btn-outline-secondary");
+    viewButton.classList.add("btn-success");
+  });
+}
+changeViewButtons()
+
+
+
